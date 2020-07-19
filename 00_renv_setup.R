618d51790d523d1629d67dc952c1cbca4ea985ad
Guilherme <guilhermeviana@gmail.com>
2020-07-19 13:05:49 -0300

    :speech_balloon: first commit

---
+++ 00_renv_setup.R
@@ -0,0 +1,5 @@
+# https://github.com/grantmcdermott/renv-rspm
+#renv::init()    ## Automatically run if you cloned/opened the repo as an RStudio project
+renv::restore()  ## Enter "y" when prompted
+options(encoding = "UTF-8",
+        repos = c(RSPM = "https://packagemanager.rstudio.com/all/latest"))
